5ef9cb159eba3037199cf913798b99f2150a2b53
Leonardo Rodrigues da Silva <leo@mac.local>
2025-10-10 23:39:11 -0300

    Implementando o loop principal

--- main.cpp
+++ main.cpp
@@ -6,17 +6,48 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <functional>
 #include "Arvore.h"
 #include "Pessoa.h"
 
 using namespace std;
 
+void print(string str){
+    cout << str << "\n";
+}
+
+struct Menu{
+    vector<string> opcoes = {
+        "1 - Adicionar pessoa",
+        "2 - Buscar pessoa",
+        "3 - Exibir parentesco",
+        "4 - Mostrar gerações",
+        "5 - Exibir Ascendentes e Descendentes",
+        "6 - Listar Arovre a partir de uma pessoa",
+        "7 - Contar Descendentes de uma Pessoa",
+        "8 - Editar Pessoa",
+        "9 - Sair",
+    };
+
+    void imprimir(){
+        for (string opc : opcoes) print(opc);
+    }
+};
+
+void processar_resposta(int resposta){
+    switch (resposta) {
+        case 1:
+            break;
+    }
+}
+
+
+
 int main()
 {
-    Arvore *arvore = new Arvore{};
-    Pessoa *pessoa = new Pessoa{"Pessoa teste", "01/01/2312", 'm'};
-
-    pessoa->mostrar();
+    Menu menu = Menu{};
+    menu.imprimir();
 
     return 0;
 }
