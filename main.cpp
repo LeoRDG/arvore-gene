1ffaf1b57f71924fbb27607a4246e6d0a2fabfbb
Leonardo Rodrigues <leonardo8571@gmail.com>
2025-10-13 01:09:30 -0300

    refactor + funcao removida

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 /**
  * Aqui rodará o programa principal
  *
- * Contém o loop principal da CLI para gerenciar pessoas
- * e relações da árvore, imprimir e salvar os dados em arquivos
+ * Contém o loop principal
  */
 
 #include <iostream>
@@ -22,44 +21,18 @@ void clear(){
     cout << "\033c";
 }
 
-void processar_resposta(int resposta)
-{
-    switch (resposta){
-    case 1:
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-    case 5:
-        break;
-    case 6:
-        break;
-    case 7:
-        break;
-    case 8:
-        break;
-    case 9:
-        break;
-
-    }
-
-}
-
 int main() {
     Arvore arvore = Arvore();
     int resposta;
-    bool valida = true;
+    bool resposta_valida = true;
 
     while (true) {
-        clear();
+        //clear();
         arvore.imprimir_opcoes();
-        if (!valida) cout << "Resposta Inválida" << "\n";
+        if (!resposta_valida) cout << "Resposta {" << resposta << "} Inválida" << "\n";
         cout << "Digite um número " << "\n";
         cin >> resposta;
-        valida = arvore.processar_resposta(resposta);
+        resposta_valida = arvore.processar_resposta(resposta);
     }
     return 0;
 }
