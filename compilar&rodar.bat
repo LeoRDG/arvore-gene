5990b551250fad262b852ebdc844164ed9f2ebe6
Leonardo Rodrigues Da Silva <04248048086@erechim.ifrs.edu.br>
2025-10-29 20:11:56 -0300

    Fix para essa bosta de windows

--- compilar&rodar.bat
+++ compilar&rodar.bat
@@ -1,13 +1,28 @@
 @echo off
+setlocal enabledelayedexpansion
 set arquivo=output.exe
 
 echo Removendo arquivo antigo..
 del /f "%arquivo%" 2>nul
+del /f *.o 2>nul
 
-echo Compilando codigo..
-g++ src\*.cpp ^
- -I include ^
- -o "%arquivo%"
+echo Compilando codigos...
 
-echo Pronto
+set OBJETOS=
+for %%f in (src\*.cpp) do (
+    echo Compilando %%f...
+    g++ -c "%%f" -I include -o "%%~nf.o"
+    set OBJETOS=!OBJETOS! "%%~nf.o"
+)
+
+echo Linkando objetos...
+g++ !OBJETOS! -o "%arquivo%"
+
+echo Limpando arquivos temporarios...
+del /f *.o 2>nul
+
+echo.
+echo Pronto! Executavel gerado: %arquivo%
 "%arquivo%"
+
+endlocal
