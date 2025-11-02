5455293eb7759fe70cf733882e8a516255197304
Leonardo Rodrigues <leonardo8571@gmail.com>
2025-11-02 18:53:59 -0300

    Melhorias nos comentarios de implementacao das funcoes de Menu

--- include/Menu.h
+++ include/Menu.h
@@ -5,20 +5,39 @@
 
 using namespace std;
 
+/**
+ * @struct Opcao
+ * @brief Representa uma opcao do menu
+ */
 struct Opcao{
-    string descricao;
-    //void(*func)();
-    function <void()> func = nullptr;
+    string descricao;                 ///< Texto descritivo da opçao exibido ao usuario
+    function<void()> func = nullptr;  ///< Funcão a ser executada quando opcão for selecionada
 };
 
+/**
+ * @brief É um menu com multiplas opcoes
+ * 
+ * Permite exibir o menu e processar a escolha do usuário.
+ */
 struct Menu{
-    vector<Opcao> opcoes;
+    vector<Opcao> opcoes;  ///< Lista de opçoes disponíveis no menu
 
+    /**
+     * @brief Imprime as opçoes do menu
+     */
     void imprimir();
 
+    /**
+     * @brief Processa a resposta do usuario e executa a funcao correspondente
+     * @param resposta Numero da opcão selecionada
+     * @returns true se a resposta foi válida e processada, se nao false
+     */
+    bool processar_resposta(int resposta);
 
-    bool processar_resposta(int);
-
-
+    /**
+     * @brief Aguarda o usuario informar uma opcao e processa a resposta
+     * 
+     * Pede um numer intiero ao usuario e chama processar_resposta() automaticamente.
+     */
     void esperar_resposta();
 };
